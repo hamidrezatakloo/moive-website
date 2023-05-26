b4d6f2feddd18cf435cae82aae89dd094464a59e
NimaSaei379 <saeinima@yahoo.com>
2023-05-26 16:45:55 +0330

    edit the _app.js file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,11 @@
-import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import Footer from "@/components/footer/Footer";
+import NavigationBar from "@/components/Navigation";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Navbar></Navbar>
+      <NavigationBar />
       <Component {...pageProps} />
       <Footer />
     </>
